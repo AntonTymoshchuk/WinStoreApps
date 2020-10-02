963383992c8485cbf4a5f84dbbeaae836770fa59
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-02 14:06:03 +0300

    Add project files.

---
+++ WinStoreApps/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.Management.Deployment;
+
+namespace WinStoreApps
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Windows.ApplicationModel.Package> packages;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PackageManager packageManager = new PackageManager();
+            packages = new ObservableCollection<Windows.ApplicationModel.Package>(packageManager.FindPackagesForUser(""));
+        }
+    }
+}
